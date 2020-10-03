b0b1cd6575b510f7c47d96b0950d9fd19a2f460a
Kassanu <kassanu@gmail.com>
2020-10-02 21:54:40 -0400

    Creates a basic changelog page

---
+++ src/components/Changelog.vue
@@ -0,0 +1,36 @@
+<template>
+  <Page id="contribute">
+    <div class="flex-grow flex justify-center">
+      <div>
+        <div>Current Version: v{{ currentVersion }}</div>
+        <Timeline :timeline-items="timelineItems" :message-when-no-items="'-'" order="desc"/>
+      </div>
+    </div>
+  </Page>
+</template>
+
+<script>
+  import Page from './Page'
+  import Timeline from 'timeline-vuejs'
+
+  export default {
+    name: 'Changelog',
+    components: {
+      Page,
+      Timeline
+    },
+    data() {
+      return {
+        currentVersion: process.env.VUE_APP_VERSION,
+        timelineItems: [
+          {
+            from: new Date(2020, 9, 1),
+            title: 'Launch',
+            description: 'Site Launched!',
+            showDayAndMonth: true
+          }
+        ]
+      }
+    }
+  }
+</script>
