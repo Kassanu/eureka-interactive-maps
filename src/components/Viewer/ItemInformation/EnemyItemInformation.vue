6432edf12d51b91e1367ace9edff8f264b29cb02
Kassanu <kassanu@gmail.com>
2020-10-15 01:05:27 -0400

    Adds aggro attack and weakness to tooltip

--- src/components/Viewer/ItemInformation/EnemyItemInformation.vue
+++ src/components/Viewer/ItemInformation/EnemyItemInformation.vue
@@ -7,6 +7,15 @@
             <Coordinates :positions="item.position"></Coordinates>
         </div>
         <div>
+            <span class="font-bold">Aggro:</span> {{ aggro }}
+        </div>
+        <div>
+            <span class="font-bold">Attack:</span> {{ attack }}
+        </div>
+        <div>
+            <span class="font-bold">Weakness:</span> {{ weakness }}
+        </div>
+        <div>
             <Drops :drops="item.drops"></Drops>
         </div>
         <div>
@@ -39,6 +48,47 @@
             skirmishes: {
                 type: Array,
                 required: true
+            }
+        },
+        computed: {
+            aggro() {
+                let agg = 'None'
+                switch (this.item.aggro) {
+                    case 'sight':
+                        agg = 'Sight'
+                        break
+                    case 'sound':
+                        agg = 'Sound'
+                        break
+                    case 'magic':
+                        agg = 'Magic'
+                        break
+                }
+                return agg
+            },
+            attack() {
+                let att = 'Both'
+                switch (this.item.attack) {
+                    case 'phyiscal':
+                        att = 'Physical'
+                        break
+                    case 'magical':
+                        att = 'Magic'
+                        break
+                }
+                return att
+            },
+            weakness() {
+                let weak = 'None'
+                switch (this.item.weakness) {
+                    case 'phyiscal':
+                        weak = 'Physical'
+                        break
+                    case 'magical':
+                        weak = 'Magic'
+                        break
+                }
+                return weak
             }
         }
     }
